5eef5da9c2c819ba38087f8e3db97c4f7ff4fc6a
Pavel28121988 <PavelTsebruk28121988@gmail.com>
2023-03-12 22:41:41 +0300

    add:project

---
+++ skript.js
@@ -0,0 +1,131 @@
+// 1
+
+// {
+//     let a = 1;
+
+//     while (a <= 50) {
+//         console.log(a);
+//         a++;
+//     }
+// }
+
+// {
+//     let a = 35;
+
+//     while (a >= 8) {
+//         console.log("   " + a);
+//         a--;
+//     }
+// }
+
+// // 2
+
+// {
+//     let a = 89;
+
+//     while (a >= 11) {
+//         document.write(a + "<br/>");
+//         a--;
+//     }
+// }
+
+// 3
+
+// {
+// let sum = 0;
+//   for (let b = 1; b <=100; b++) {
+    
+//     console.log (sum + "+" +  b + "=" +(sum+b));
+//     sum += b;
+//    console.log (sum);
+  
+// }
+// }
+
+// 4 
+
+// let sum =0;
+
+// for (let c=1; c<=5; c++) {
+//     sum = 0;
+
+//     for (let d=1; d<=c; d++) {
+//         sum +=d;
+//     }
+//     console.log ("Sum " + c + " = " + sum)
+// }
+  
+
+// 5 
+
+// {
+//     for (let e = 8; e <= 56; e++ ) { 
+//         if (e % 2 == 0 ) 
+//         console.log (e);
+//     }
+// }
+
+// { 
+//     let f = 8;
+//     while (f <=56) {
+//     f++;
+//     if (f % 2==0)
+//     console.log  (f);
+//     }  
+// }
+
+// 6  
+
+// {
+// for (let  g = 1; g <=10; g++ ){
+//     for (let h =1; h <=10; h++ )
+
+//     document.write(g +  " * "  + h +  " = " + (g*h) + "<br/>" )
+// }
+// }
+
+// 7
+// {
+// let num = 0
+// for (let n = 1000; n > 50;  n /= 2) {
+//     console.log (n);
+//     console.log ("Количество Интераций  " + ( num = (n/n)+num));
+// }
+
+// }
+
+// 8
+
+
+// {
+//     let w = 0
+
+//     while (true) {
+//         let num = +prompt("Введите чило:");
+//         if (num === 0) break;
+//         elso ()
+//     }
+// }
+
+// {
+//     let total = 0;
+//     let count = 0;
+
+//     for (;;){
+//         let num = +prompt("Введите чило:");
+//         if (num ===0 )break;
+//         if (isFinite(num)){
+//             total += num;
+//             count++
+//         } else{
+//             alert("Это не число")
+//         }
+//     }
+
+//     console.log(total);
+//     console.log(total/count);
+// }
+
+
+// 9 // 10 не разобрался.
+
